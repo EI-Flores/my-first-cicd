d8277879a2cf74c2c616eb86de177744899f05a2
Juan Arturo Flores Rivera <ing.flores.rivera@gmail.com>
2026-03-28 20:01:20 -0600

    First commit: basic calculator and CI workflow

---
+++ main.py
@@ -0,0 +1,6 @@
+from calculator import add, subtract
+
+if __name__ == "__main__":
+    print("--- Calculadora Simple ---")
+    resultado = add(10, 20)
+    print(f"La suma de 10 + 20 es: {resultado}")
